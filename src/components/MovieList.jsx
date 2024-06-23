b587038647d305578503720bd5ee0513caaea465
sameer2407 <satyamjadaun2001@gmail.com>
2024-06-23 12:09:42 +0530

    carousels

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -14,15 +14,15 @@ const MovieList = ({ title, movies }) => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 5,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1,
+      items: 3,
     },
   };
 
